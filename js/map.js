7dd6c0058e5e00806b45f0877e489c177e5b9391
Wensenter <18365206169@163.com>
2020-11-06 17:46:43 +0800

    test canvas

--- js/map.js
+++ js/map.js
@@ -26,14 +26,17 @@
         logoControl: false
     });
 
-    let baseSize = 1000000,size = 80;
+    let baseSize = 1000000,size = 80,mapLoading = false;
     const MAX_SIZE = 2.5,MIN_SIZE = 1;
+
     map.on('load',function(){
-        addSources();
+        addGanSuGeometry() //绘制甘肃区域高亮
+        mapLoading = true
     })
 
-    let numSize,sizeArr = [];
-    function addSources(){
+
+    //绘制甘肃区域高亮
+    function addGanSuGeometry() {
         $.ajax({
             url:'../json/geoJson.json',
             type:'GET',
@@ -42,9 +45,27 @@
                     "type": "geojson",
                     "data": res
                 });
-                addLayers()
+                map.addLayer({
+                    "id": "fillLayer",
+                    "type": "fill",
+                    "source": "fillSource",
+                    "layout": {
+                        "visibility": "visible",
+                    },
+                    "paint": {
+                        "fill-color": "#1773B4",
+                        "fill-opacity": 0.8,
+                        "fill-outline-color": "#017FA7"
+                    },
+                });
+                addScatterSources();
             }
         })
+    }
+
+    let numSize,sizeArr = [];
+    // 首次加载
+    function addScatterSources(){
         $.ajax({
             url:"../json/city.json",
             type:"get",
@@ -72,32 +93,24 @@
                         }
                     })
                 })
-               sizeArr.sort()
+                sizeArr.sort()
                 map.addSource('points', {
                     'type': 'geojson',
                     'data': {
                         'type': 'FeatureCollection',
                         'features': jsonFeatures
-                    }
-                });                
+                    },
+                    // cluster: true,
+                    // clusterMaxZoom: 15, /* 最大聚合层级 */
+                    // clusterRadius: 50, /* 聚合半径 */
+                });   
+                
+                addScatterLayers()
             }
         })
     }
 
-    function addLayers() {
-        map.addLayer({
-            "id": "fillLayer",
-            "type": "fill",
-            "source": "fillSource",
-            "layout": {
-                "visibility": "visible",
-            },
-            "paint": {
-                "fill-color": "#1773B4",
-                "fill-opacity": 0.8,
-                "fill-outline-color": "#017FA7"
-            },
-        });
+    function addScatterLayers() {
         map.addImage('pulsing-dot', pulsingDot, {
             pixelRatio: 2
         });
@@ -118,6 +131,102 @@
                 "text-allow-overlap": true,   //图标覆盖文字允许压盖
             }
         });
+    }
+
+    // 户籍分布图
+    function addScatter1Sources() {
+        map.removeSource('points')
+        $.ajax({
+            url:"../json/city.json",
+            type:"get",
+            success:function(res){
+                let {rows} = res
+                let jsonFeatures = [];
+                rows.map(e=>{
+                    numSize = (e.people_count_2010/baseSize).toFixed(2)*1;
+                    if(numSize < MIN_SIZE){
+                        numSize = MIN_SIZE
+                    }
+                    if(numSize > MAX_SIZE){
+                        numSize = MAX_SIZE
+                    }
+                    sizeArr.push([numSize,numSize])
+                    jsonFeatures.push({
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [e.lng,e.lat]
+                        },
+                        "properties": {
+                            "kind": numSize,
+                            "title":(numSize*100).toFixed(0,10)
+                        }
+                    })
+                })
+                sizeArr.sort()
+                map.addSource('points', {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': jsonFeatures
+                    },
+                    // cluster: true,
+                    // clusterMaxZoom: 15, /* 最大聚合层级 */
+                    // clusterRadius: 50, /* 聚合半径 */
+                });   
+                
+                addScatter1Layers()
+            }
+        })
+    }
+
+    function addScatter1Layers() {
+        map.removeImage('pulsing-dot')
+        map.removeLayer('points')
+        map.addImage('pulsing-dot', pulsingDot, {
+            pixelRatio: 2
+        });
+        map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'points',
+            'layout': {
+                "text-field": "{title}",
+                'icon-image': 'pulsing-dot',
+                "icon-size": {
+                    "property":"kind",
+                    "base":1,
+                    "stops":sizeArr
+                },
+                "text-size":14,
+                "icon-allow-overlap": true,  //图标允许压盖
+                "text-allow-overlap": true,   //图标覆盖文字允许压盖
+            }
+        });
+    }
+
+    // 赴京流向图
+    let echartslayer = minemap.Template.create({map: map, type: 'od'});
+    function addLine1Sources() {
+        if(!mapLoading) return
+        $.ajax({
+            url:'../json/line1.json',
+            type:'get',
+            success:function(res){
+                let {data} = res;
+                data.map(e=>{
+                    to = '北京';
+                    e.unshift(40.2539)
+                    e.unshift(116.4551)
+                    e.splice(4,0,to)
+                })
+               let series = getSeries(data)
+               let option = getOption(series);
+
+               map.zoomTo(3)
+                echartslayer.chart.setOption(option);
+            }
+        })
     }
 
      // 定义一个CustomLayerInterface来在地图上绘制脉冲点
@@ -186,3 +295,132 @@
             return true;
         }
     };
+
+    // 飞线图数据处理
+    function getSeries(data) {
+        var scatterData = [];
+        var lineData = [];
+        var min = Number.MAX_VALUE;
+        var max = Number.MIN_VALUE;
+
+        for (var i = 0; i < data.length; i++) {
+            var item = data[i];
+            var name = item[4];
+            var dest = item[5];
+            var count = item[6];
+
+            if (count < min) {
+                min = count;
+            }
+            if (count > max) {
+                max = count;
+            }
+            scatterData.push({
+                name: dest,
+                count: count,
+                value: item.slice(2, 4)
+            });
+            lineData.push({
+                name: name,
+                count: count,
+                dest: dest,
+                coords: [item.slice(0, 2), item.slice(2, 4)]
+            });
+        }
+
+        return [{
+            name: 'bgLine',
+            type: 'lines',
+            coordinateSystem: 'GLMap',
+            zlevel: 1,
+            lineStyle: {
+                normal: {
+                    color: '#FFF800',
+                    width: 2,
+                    opacity: 0.5,
+                    curveness: 0.2
+                }
+            },
+            data: lineData
+        }, {
+            name: 'scatter',
+            type: 'scatter',
+            coordinateSystem: 'GLMap',
+            zlevel: 2,
+
+            label: {
+                normal: {
+                    show: true,
+                    position: 'right',
+                    formatter: '{b}'
+                }
+            },
+            symbolSize: 10,
+            itemStyle: {
+                normal: {
+                    show: true,
+                    color: '#FFF800'
+                }
+            },
+            data: scatterData
+        }, {
+            name: 'sLine',
+            type: 'lines',
+            coordinateSystem: 'GLMap',
+            zlevel: 3,
+            effect: {
+                show: true,
+                period: 6,
+                trailLength: 0.4,
+                symbolSize: 4
+            },
+            lineStyle: {
+                normal: {
+                    color: '#FFF800',
+                    width: 0,
+                    curveness: 0.2
+                }
+            },
+            data: lineData
+        }, {
+            name: 'lLine',
+            type: 'lines',
+            coordinateSystem: 'GLMap',
+            zlevel: 4,
+            effect: {
+                show: true,
+                period: 6,
+                trailLength: 0.4,
+                opacity: 0.08,
+                symbolSize: 15
+            },
+            lineStyle: {
+                normal: {
+                    color: '#FFF800',
+                    width: 0,
+                    curveness: 0.2
+                }
+            },
+            data: lineData
+        }
+        ];
+    }
+
+    // 设置echatrs Option
+    function getOption(series) {
+        return {
+            GLMap: {
+                roam: true
+            },
+            tooltip: {
+                trigger: 'item',
+                backgroundColor: 'rgba(0,0,0,0)',
+                formatter: function (param) {
+                    return param.data.name + '->' + param.data.dest + ': ' + param.data.count;
+                }
+            },
+            series: series
+        };
+    }
+
+    //
